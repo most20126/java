056696726e2663fa46716e992b0dfe67aa56aaea
most20126 <106127630+most20126@users.noreply.github.com>
2022-05-31 08:54:06 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
 class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! what i'm doing"); 
+        System.out.println("NAME       : Itsara"); 
+        System.out.println("NICKNAME   : Mos"); 
+        System.out.println("DEPARTMENT : ICT"); 
+        System.out.println("GRADE      : 3.77"); 
     }
 }
